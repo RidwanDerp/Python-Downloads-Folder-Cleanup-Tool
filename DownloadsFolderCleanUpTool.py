e22cb2574cd0d736949f8a46b929fd78e3f807fd
RidwanDerp <ridwanhaque.0942@gmail.com>
2024-07-07 18:15:01 -0400

    Update DownloadsFolderCleanUpTool.py
    Configured a Test Folder creation program, Test was successful.

--- DownloadsFolderCleanUpTool.py
+++ DownloadsFolderCleanUpTool.py
@@ -1,6 +1,36 @@
+import os
+
+
 AUDIO = ['mp3','wav','raw','wma','mid','midi']
 VIDEO = ['mp4','mpg','mpeg','avi','mov','flv','mkv','mwv','m4v','h264']
 IMAGES = ['png','jpg','jpeg','gif','svg','bmp','psd','tiff','tif']
 DOCS = ['txt','pdf','csv','xls','xlsx','ods','doc','docx','html','odt','tex','ppt','pptx','log']
 COMPR = ['zip','z','7z','rar','tar','gz','pkg','deb','rpm']
 INSTL = ['dmg','exe','iso']
+
+
+#creating directories 
+
+BASE_PATH = os.path.expanduser('~')
+DEST_DIRS = ['tester folder']
+
+
+for d in DEST_DIRS:
+    dir_path = os.path.join(BASE_PATH, d)
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
