0ac985cb897c169841131df6bbe78ee4567f4513
RidwanDerp <ridwanhaque.0942@gmail.com>
2024-07-07 19:09:44 -0400

    Directory pprint Test
    File Mapping: The script then maps files from the ‘Downloads’ directory to their respective file extensions. It does this by listing all files in the ‘Downloads’ directory, splitting each filename to get the file extension, and then appending the filename to the corresponding list in a dictionary (files_mapping). The dictionary uses the file extension as the key and a list of filenames with that extension as the value.
    
    Printing the Result: Finally, it pretty-prints the files_mapping dictionary. This gives you a clear, readable output of which files in the ‘Downloads’ directory have which extensions.

--- DownloadsFolderCleanUpTool.py
+++ DownloadsFolderCleanUpTool.py
@@ -1,5 +1,8 @@
 import os
-
+#create data structure to map all files within folder
+import collections
+#better printing
+from pprint import pprint
 
 AUDIO = ['mp3','wav','raw','wma','mid','midi']
 VIDEO = ['mp4','mpg','mpeg','avi','mov','flv','mkv','mwv','m4v','h264']
@@ -9,10 +12,10 @@ COMPR = ['zip','z','7z','rar','tar','gz','pkg','deb','rpm']
 INSTL = ['dmg','exe','iso']
 
 
-#creating directories 
+#creating directories for where we want to store the downloaded files
 
 BASE_PATH = os.path.expanduser('~')
-DEST_DIRS = ['tester folder']
+DEST_DIRS = ['Downloads Organizer']
 
 
 for d in DEST_DIRS:
@@ -20,6 +23,20 @@ for d in DEST_DIRS:
     if not os.path.isdir(dir_path):
         os.mkdir(dir_path)
 
+#map files from downloads folder 
+DOWNLOADS_PATH = os.path.join(BASE_PATH, 'Downloads')
+files_mapping = collections.defaultdict(list)
+files_list = os.listdir(DOWNLOADS_PATH)
+
+
+
+
+for file_name in files_list:
+    file_ext = file_name.split('.')[-1]  # Get the last element after splitting
+    files_mapping[file_ext].append(file_name)
+
+
+pprint(files_mapping)
 
 
 
