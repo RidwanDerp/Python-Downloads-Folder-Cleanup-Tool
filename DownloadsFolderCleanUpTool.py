67bdead5648a55524d159a99a881c66aaf6e4831
RidwanDerp <ridwanhaque.0942@gmail.com>
2024-07-14 16:17:41 -0400

    Update
    Downloads Organizer folder is created and houses the destination directories

--- DownloadsFolderCleanUpTool.py
+++ DownloadsFolderCleanUpTool.py
@@ -1,7 +1,5 @@
 import os
-#create data structure to map all files within folder
 import collections
-#better printing
 from pprint import pprint
 
 AUDIO = ['mp3','wav','raw','wma','mid','midi']
@@ -11,70 +9,52 @@ DOCS = ['txt','pdf','csv','xls','xlsx','ods','doc','docx','html','odt','tex','pp
 COMPR = ['zip','z','7z','rar','tar','gz','pkg','deb','rpm']
 INSTL = ['dmg','exe','iso']
 
-
-#creating directories for where we want to store the downloaded files
-
+# Creating directories for where we want to store the downloaded files
 BASE_PATH = os.path.expanduser('~')
+ORGANIZER_DIR = 'Downloads Organizer'
 DEST_DIRS = ['Music','Movies','Pictures','Documents','Applications','Other']
 
+# Create the Downloads Organizer directory if it doesn't exist
+organizer_path = os.path.join(BASE_PATH, ORGANIZER_DIR)
+if not os.path.isdir(organizer_path):
+    os.mkdir(organizer_path)
 
+# Create the destination directories inside the Downloads Organizer directory
 for d in DEST_DIRS:
-    dir_path = os.path.join(BASE_PATH, d)
+    dir_path = os.path.join(organizer_path, d)
     if not os.path.isdir(dir_path):
         os.mkdir(dir_path)
 
-#map files from downloads folder 
+# Map files from downloads folder 
 DOWNLOADS_PATH = os.path.join(BASE_PATH, 'Downloads')
 files_mapping = collections.defaultdict(list)
 files_list = os.listdir(DOWNLOADS_PATH)
-
-
-
 
 for file_name in files_list:
     file_ext = file_name.split('.')[-1]  # Get the last element after splitting
     files_mapping[file_ext].append(file_name)
 
-
 pprint(files_mapping)
 
-#move all files with to a target destination folder in accordance with their file type
-
+# Move all files with to a target destination folder in accordance with their file type
 for f_ext, f_list in files_mapping.items():
-
-
     if f_ext in INSTL:
         for file in f_list:
-            os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Applications', file))
+            os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(organizer_path, 'Applications', file))
             #print(os.path.join(DOWNLOADS_PATH, file))
-            #print(os.path.join(BASE_PATH, 'Applications', file))
-    
+            #print(os.path.join(organizer_path, 'Applications', file))
     elif f_ext in AUDIO:
         for file in f_list:
-            os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Music', file))
-
+            os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(organizer_path, 'Music', file))
     elif f_ext in VIDEO:
         for file in f_list:
-            os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Movies', file))
-
+            os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(organizer_path, 'Movies', file))
     elif f_ext in IMAGES:
         for file in f_list:
-            os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Pictures', file))
-
+            os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(organizer_path, 'Pictures', file))
     elif f_ext in DOCS:
         for file in f_list:
-            os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Documents', file))
-
+            os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(organizer_path, 'Documents', file))
     else:
         for file in f_list:
-            os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Other', file))
-
-
-
-
-
-
-
-
-
-        
+            os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(organizer_path, 'Other', file))
